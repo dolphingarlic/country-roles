3b663bb4c2ca8e122aba1461681d4c79505e0e5c
Andi Qu <andi@andiqu.com>
2020-07-11 18:17:11 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+import logging
+
+from discord import Activity, ActivityType
+from discord.ext.commands import Bot, when_mentioned_or
+
+from cogs.country_roles import CountryRoles
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    prefix = os.environ.get('BOT_PREFIX', 'r!')
+    bot = Bot(
+        command_prefix=when_mentioned_or(prefix),
+        activity=Activity(type=ActivityType.playing, name=f'with roles | {prefix}help'),
+    )
+    bot.add_cog(CountryRoles(bot))
+    await bot.start(os.environ['DISCORD_TOKEN'])
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        loop.close()
---
+++ cogs/country_roles.py
@@ -0,0 +1,58 @@
+from random import randint
+
+import discord
+from discord.ext.commands import Cog, command, has_permissions
+from discord.utils import get
+
+from cogs.countries import COUNTRIES, FLAGS
+
+
+class CountryRoles(Cog):
+    def __init__(self, bot):
+        """Initializes the cog's bot"""
+        self.bot = bot
+
+    @command(description='Adds all country roles and sets up react messages')
+    @has_permissions(administrator=True)
+    async def start(self, ctx):
+        try:
+            guild = ctx.guild
+            guild_roles = await guild.fetch_roles()
+            for country in COUNTRIES:
+                already_added = any(
+                    role.name == country for role in guild_roles)
+                if not already_added:
+                    await guild.create_role(name=country, colour=discord.Colour(randint(0, 0xffffff)))
+        except:
+            print('Couldn\'t add all roles :(')
+
+    @command(description='Deletes all previously added country roles')
+    @has_permissions(administrator=True)
+    async def reset(self, ctx):
+        try:
+            guild = ctx.guild
+            guild_roles = await guild.fetch_roles()
+            added_country_roles = filter(
+                lambda x: x.name in COUNTRIES, guild_roles)
+            for role in added_country_roles:
+                await role.delete()
+        except:
+            print('Couldn\'t delete all roles :(')
+
+    @command(description='Gives the user the specified role')
+    async def gimme(self, ctx, country):
+        try:
+            guild = ctx.guild
+            guild_roles = await guild.fetch_roles()
+            user_roles = list(
+                filter(lambda x: x.name not in COUNTRIES, ctx.message.author.roles))
+
+            role = get(guild_roles, name=country)
+            if role is not None:
+                user_roles.append(role)
+            if country in FLAGS:
+                user_roles.append(get(guild_roles, name=FLAGS[country]))
+
+            await ctx.message.author.edit(roles=user_roles)
+        except:
+            print('Couldn\'t assign role :(')
